48a02ae55cdc3b0e7862f442d87639ab12188d86
Alex Musichen <alex.musichen@gmail.com>
2016-06-13 00:19:45 +0200

    added Spreadsheet support in popup form. (initial)

--- free_trial_order.php
+++ free_trial_order.php
@@ -1,10 +1,15 @@
 <?php
+//if "email" variable is filled out, send email
+if (isset($_REQUEST['email']))  {
+    //Email information
+    $admin_email = "alexander.steglin@gmail.com";
+    $email = $_REQUEST['email'];
+    $first_name = $_REQUEST['first_name'];
+    $last_name = $_REQUEST['first_name'];
 
-if($_POST)
-{
-    $date=$_POST['date'];
-    $time=$_POST['time'];
-    mysql_query("SQL insert statement.......");
-}else { }
+    //send email
+    mail($admin_email, "$first_name", $first_name, "From:" . $email);
 
-?>
+    //Email response
+    echo "Thank you for contacting us!";
+}
